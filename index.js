15127e9dc84e91be4769c54c994a726fa21caac9
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2021-04-17 15:15:34 +0200

    changing goals

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const app = require('express')();
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const users = JSON.parse(fs.readFileSync('users.json'));
 const bankAccounts = JSON.parse(fs.readFileSync('bankAccounts.json'));
@@ -31,6 +34,17 @@ app.get('/bank-accounts/owner/:id', async (req, res) => {
 
 app.get('/categories/user/:id', async (req,res) => {
     res.send(categories.find(({user}) => user == req.params.id).categories);
+})
+
+// housing, transportation, food, utilities, insurance, healthcare, investing, hobby
+app.put('/goals/:id', async (req, res) => {
+    const userCategories = categories.find(({user}) => user == req.params.id).categories;
+    const changedCategories = req.body;
+    changedCategories.forEach((el) => {
+        userCategories.find(({category}) => category == el.category).spendingLimit = el.spendingLimit;
+    });
+
+    res.send(userCategories);
 })
 
 app.get('/months/user/:id', async (req,res) => {
